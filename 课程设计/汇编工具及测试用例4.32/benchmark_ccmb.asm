d7c23f4db583614a1d465b41cf829cdaf2cd3bc1
Your Name <chenyanliu712@gmail.com>
2020-02-24 16:51:22 +0800

    重定向流水线完成

--- 课程设计/汇编工具及测试用例4.32/benchmark_ccmb.asm
+++ 课程设计/汇编工具及测试用例4.32/benchmark_ccmb.asm
@@ -299,23 +299,110 @@ sort_next:
 #############################################
 # insert your ccmb benchmark program here!!!
 #############################################
+#C1 instruction benchmark:LUI
+#依次输出 0xfedcffff 0xba980000 0x76540000 0x32100000 
+#0xfedcffff 0xba980000 0x76540000 0x32100000 
+#0xfedcffff 0xba980000 0x76540000 0x32100000 
+#0xfedcffff 0xba980000 0x76540000 0x32100000 
+#0xfedcffff 0xba980000 0x76540000 0x32100000 
+#0xfedcffff 0xba980000 0x76540000 0x32100000 
+#0xfedcffff 0xba980000 0x76540000 0x32100000 
+#0xfedcffff 0xba980000 0x76540000 0x32100000
+.text
 
-j benchmark_start       #delete this instruction for ccmb bencmark
-#C1 instruction benchmark
+addi $t3,$zero,  0x8
+
+lui_branch:
+lui $s1,  0xFEDC 
+ori $s1,$s1, 0xffff
+add $a0,$0,$s1          
+addi $v0,$zero,34         # system call for print
+syscall    
+lui $s1,  0xBA98
+add $a0,$0,$s1          
+syscall    
+lui $s1,  0x7654     
+add $a0,$0,$s1          
+syscall    
+lui $s1,  0x3210     
+add $a0,$0,$s1          
+syscall    
+                           # print
+addi $t3,$t3, -1    
+bne $t3,$zero,lui_branch
+
+
+#C2 instruction benchmark:SLTIU
+#依次输出  0x00001997 0x00001996 0x00001995 0x00001994 0x00001993 0x00001992 0x00001991 0x00001990 
+#0x0000198f 0x0000198e 0x0000198d 0x0000198c 0x0000198b 0x0000198a 0x00001989 0x00001988 0x00001987 
+#0x00001986 0x00001985 0x00001984 0x00001983 0x00001982 0x00001981 0x00001980 0x0000197f 0x0000197e 
+#0x0000197d 0x0000197c 0x0000197b 0x0000197a 0x00001979 0x00001978 0x00001977 0x00001976 0x00001975 
+#0x00001974 0x00001973 0x00001972 0x00001971 0x00001970 0x0000196f 0x0000196e 0x0000196d 0x0000196c 
+#0x0000196b 0x0000196a 0x00001969 0x00001968 0x00001967 0x00001966 0x00001965 0x00001964 0x00001963 
+#0x00001962 0x00001961 0x00001960 0x0000195f 0x0000195e 0x0000195d 0x0000195c 0x0000195b 0x0000195a 
+#0x00001959 0x00001958 0x00001957 0x00001956 0x00001955 0x00001954 0x00001953 0x00001952 0x00001951 
+#0x00001950 0x0000194f 0x0000194e 0x0000194d 0x0000194c 0x0000194b 0x0000194a 0x00001949
+.text
+addi $t0,$zero,-1    
+addi $t1,$zero,0     
+addi $s1,$zero, 0x1997  
+sltiu_branch:
+add $a0,$0,$s1          
+addi $v0,$zero,34        # system call for print
+syscall                  # print
+add $s1,$s1,$t0     
+sltiu $t1,$s1, 0x1949
+beq $t1,$zero,sltiu_branch
+
+
+#Mem instruction benchmark:LB
+#依次输出 0xffffff81 0xffffff82 0xffffff83 0xffffff84 0xffffff85 0xffffff86 0xffffff87 
+#0xffffff88 0xffffff89 0xffffff8a 0xffffff8b 0xffffff8c 0xffffff8d 0xffffff8e 0xffffff8f 
+#0xffffff90 0xffffff91 0xffffff92 0xffffff93 0xffffff94 0xffffff95 0xffffff96 0xffffff97 
+#0xffffff98 0xffffff99 0xffffff9a 0xffffff9b 0xffffff9c 0xffffff9d 0xffffff9e 0xffffff9f 
+#0xffffffa0
+.text
+addi $t1,$zero,0     #init_addr 
+addi $t3,$zero,16     #counter
+
+#预先写入数据，实际是按字节顺序存入 0x81,82,84,86,87,88,89.......等差数列
+ori $s1,$zero, 0x8483  #
+addi $s2,$zero, 0x0404  #
+sll $s1,$s1,16
+sll $s2,$s2,16
+ori $s1,$s1, 0x8281  #    注意一般情况下MIPS采用大端方式
+addi $s2,$s2, 0x0404  #   init_data= 0x84838281 next_data=init_data+ 0x04040404
+lb_store:
+sw $s1,($t1)
+add $s1,$s1,$s2   #data +1
+addi $t1,$t1,4    # addr +4  
+addi $t3,$t3,-1   #counter
+bne $t3,$zero,lb_store
+
+addi $t3,$zero,32   #循环次数
+addi $t1,$zero,0    # addr 
+lb_branch:
+lb $s1,($t1)         #测试指令
+add $a0,$0,$s1          
+addi $v0,$zero,34         #输出
+syscall                  
+addi $t1,$t1, 1    
+addi $t3,$t3, -1    
+bne $t3,$zero,lb_branch
 
 
 
-#C2 instruction benchmark
-
-
-
-#Mem instruction benchmark
-
-
-
-
-#Branch instruction benchmark
-
+#Branch instruction benchmark:BLTZ
+#依次输出0xfffffff1 0xfffffff2 0xfffffff3 0xfffffff4 0xfffffff5 0xfffffff6 0xfffffff7 
+#0xfffffff8 0xfffffff9 0xfffffffa 0xfffffffb 0xfffffffc 0xfffffffd 0xfffffffe 0xffffffff
+.text
+addi $s1,$zero,-15       #初始值
+bltz_branch:
+add $a0,$0,$s1          # $a0=-15
+addi $v0,$zero,34         
+syscall                  #输出当前值
+addi $s1,$s1,1 
+bltz $s1,bltz_branch     #当前指令
 
                  
  addi   $v0,$zero,10         # system call for exit
